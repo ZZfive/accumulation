15a30f9b92df61c012d525dfa2bffddb4526205e
zjy <626958258@qq.com>
2024-04-05 22:48:27 +0800

    end part7 of fastapi

--- server/fastapi/part7.py
+++ server/fastapi/part7.py
@@ -4,16 +4,19 @@
  共享业务逻辑，复用相同的代码逻辑
  共享数据库连接
  实现安全、验证、角色限权等
+
+
+依赖注入无非是与路径操作函数一样的函数罢了。但它依然非常强大，能够声明任意嵌套深度的「图」或树状的依赖结构
 '''
 
 from typing import Union
 
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, Cookie
 
 app = FastAPI()
 
 
-# 依赖项就是一个函数，且可以使用与路径操作函数相同的参数；形式和结构与路径操作函数一样，可以把依赖项当作没有装饰器（即，没有 @app.get("/some-path") ）的路径操作函数。
+# 依赖项最常见的形式是一个函数，且可以使用与路径操作函数相同的参数；形式和结构与路径操作函数一样，可以把依赖项当作没有装饰器（即，没有 @app.get("/some-path") ）的路径操作函数。
 '''
 依赖项可以返回各种内容。本例中的依赖项预期接收如下参数：
   类型为 str 的可选查询参数 q
@@ -51,6 +54,70 @@ FastAPI 调用依赖项的方式与路径操作函数一样，因此，定义依
 
 详情链接：https://fastapi.tiangolo.com/zh/tutorial/dependencies/
 '''
+
+
+''''
+依赖项应该是一个可调用对象，但不仅限于函数，可以用类作为依赖项，python中类是一个可调用对象
+如果在 FastAPI 中传递一个 "可调用对象" 作为依赖项，它将分析该 "可调用对象" 的参数，并以处理路径操作函数的参数的方式来处理它们。包括子依赖项。
+这也适用于完全没有参数的可调用对象。这与不带参数的路径操作函数一样。所以，可以将上面的依赖项 "可依赖对象" common_parameters 更改为类 CommonQueryParams
+'''
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+class CommonQueryParams:
+    def __init__(self, q: str | None = None, skip: int = 0, limit: int = 100):  # 与common_parameters函数参数相同，这些参数就是fastapi用于处理依赖项的
+        self.q = q
+        self.skip = skip
+        self.limit = limit
+
+
+'''
+以下声明依赖项时，对参数commons定义中，第一个CommonQueryParams进行类型声明，但其对fastapi来说没有任何意义，FastAPI 不会使用它进行数据转换、验证等 (因为对于这，它使用 = Depends(CommonQueryParams))
+其实可以直接如此定义：commons = Depends(CommonQueryParams)；但声明类型是被鼓励的，便于编辑器指导参数具体类型，
+可以按如下定义：commons: CommonQueryParams = Depends()
+'''
+@app.get("/items/")
+# async def read_items(commons: CommonQueryParams = Depends(CommonQueryParams)):  # 声明依赖项；FastAPI 调用 CommonQueryParams 类。这将创建该类的一个 "实例"，该实例将作为参数 commons 被传递给函数
+# async def read_items(commons = Depends(CommonQueryParams)):
+async def read_items(commons: CommonQueryParams = Depends()):
+    response = {}
+    if commons.q:
+        response.update({"q": commons.q})
+    items = fake_items_db[commons.skip : commons.skip + commons.limit]
+    response.update({"items": items})
+    return response
+
+
+'''
+子依赖项：FastAPI 支持创建含子依赖项的依赖项。并且，可以按需声明任意深度的子依赖项嵌套层级。FastAPI 负责处理解析不同深度的子依赖项
+'''
+def query_extractor(q: Union[str, None] = None):  # 第一层依赖项；声明了类型为 str 的可选查询参数 q，然后返回这个查询参数
+    return q
+
+
+def query_or_cookie_extractor(  # 第二层依赖项
+    q: str = Depends(query_extractor),  # 依赖嵌套；该函数依赖 query_extractor, 并把 query_extractor 的返回值赋给参数 q
+    last_query: Union[str, None] = Cookie(default=None),  # 类型是 str 的可选 cookie（last_query）用户未提供查询参数 q 时，则使用上次使用后保存在 cookie 中的查询
+):
+    if not q:
+        return last_query
+    return q
+
+
+@app.get("/items/")
+async def read_query(query_or_default: str = Depends(query_or_cookie_extractor)):  # 使用依赖项；注意，这里在路径操作函数中只声明了一个依赖项，即 query_or_cookie_extractor 。但 FastAPI 必须先处理 query_extractor，以便在调用 query_or_cookie_extractor 时使用 query_extractor 返回的结果
+    return {"q_or_cookie": query_or_default}
+
+
+'''
+如果在同一个路径操作 多次声明了同一个依赖项，例如，多个依赖项共用一个子依赖项，FastAPI 在处理同一请求时，只调用一次该子依赖项。
+FastAPI 不会为同一个请求多次调用同一个依赖项，而是把依赖项的返回值进行「缓存」，并把它传递给同一请求中所有需要使用该返回值的「依赖项」。
+
+在高级使用场景中，如果不想使用「缓存」值，而是为需要在同一请求的每一步操作（多次）中都实际调用依赖项，可以把 Depends 的参数 use_cache 的值设置为 False :
+'''
+async def needy_dependency(fresh_value: str = Depends(get_value, use_cache=False)):
+    return {"fresh_value": fresh_value}
+
 
 if __name__ ==  "__main__":
     import uvicorn
